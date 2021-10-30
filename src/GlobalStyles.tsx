c83c57d3ff3dec5becac90f9e098e207ba5296ab
electro2209 <20400660+manuelc2209@users.noreply.github.com>
2021-10-30 20:44:37 +0100

    Formatted files to new prettier rules

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,12 +1,12 @@
-import { css } from "styled-components";
+import { css } from 'styled-components';
 
 export const fontStyle = css`
-  font-size: 14px;
-  font-weight: 500;
-  color: #111;
-  font-family: "Helvetica Neue", sans-serif;
-  letter-spacing: -1px;
-  line-height: 1;
+    font-size: 14px;
+    font-weight: 500;
+    color: #111;
+    font-family: 'Helvetica Neue', sans-serif;
+    letter-spacing: -1px;
+    line-height: 1;
 `;
 
-export const lightgrey1 = "#c5c5c5";
+export const lightgrey1 = '#c5c5c5';
--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Button } from ".";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from '.';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Button />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
