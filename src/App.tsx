8f37bcf53f45bcac045b3824c9b9bbe344a1da3a
electro2209 <20400660+manuelc2209@users.noreply.github.com>
2021-10-30 20:41:09 +0100

    Updated UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
-import { Login } from "./components/ui/login";
+import React, { useState } from 'react';
+import { Login } from './components/ui/login';
 
 export const App: React.FC = () => {
-  const [disabled, setDisabled] = useState(false);
+    const [disabled, setDisabled] = useState(false);
 
-  const handleOnClick = () => {
-    //setDisabled(true);
-  };
+    const handleOnClick = () => {
+        setDisabled(true);
+    };
 
-  return (
-    <div>
-      <Login
-        nameLabel="Username:"
-        nicknamePlaceholder="Please insert a valid Nickname here:"
-        passwordLabel="Password:"
-        passwordPlaceholder="Please insert a valid password here:"
-        validatePassword={true}
-        onClick={handleOnClick}
-        disabled={disabled}
-      ></Login>
-    </div>
-  );
+    return (
+        <div>
+            <Login
+                nameLabel="Username:"
+                nicknamePlaceholder="Please insert a valid Nickname here:"
+                passwordLabel="Password:"
+                passwordPlaceholder="Please insert a valid password here:"
+                validatePassword={true}
+                onClick={handleOnClick}
+                disabled={disabled}
+            ></Login>
+        </div>
+    );
 };
