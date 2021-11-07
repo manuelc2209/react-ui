ea73895b7e85de63875073a97dc4ea11a148faa8
electro2209 <20400660+manuelc2209@users.noreply.github.com>
2021-11-07 23:16:16 +0000

    Updated skills tree & json properties

--- src/components/ui/skills/index.tsx
+++ src/components/ui/skills/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
+type skillData = {
+    skillLevel: number;
+    label: string;
+    logo: string;
+};
+
+interface SkillsProps {
+    skills?: skillData[];
+}
+
+interface StyledSkillContainerProps {
+    percentage?: number;
+}
+
+const setPercentage = ({ percentage }: { percentage?: number }) =>
+    `linear-gradient(to right, #3d3d3d ${percentage}%, #000000 ${percentage}%)`;
+
+const StyledSkillContainer = styled.div<StyledSkillContainerProps>`
+    background: ${setPercentage};
+`;
+
 const StyledLabel = styled.span`
     color: white;
 `;
@@ -13,13 +34,27 @@ const StyledWrapper = styled.div`
 
     ${StyledLabel} {
         text-align: center;
+        margin-bottom: 10px;
+    }
+
+    ${StyledSkillContainer} {
+        margin: 20px;
     }
 `;
 
-export const Skills: React.FC = ({}) => {
+export const Skills: React.FC<SkillsProps> = ({ skills }) => {
+    const filteredSkills = skills && skills.sort((item, acc) => item.skillLevel - acc.skillLevel);
+
     return (
         <StyledWrapper>
-            <StyledLabel>Skills</StyledLabel>
+            <StyledLabel>My skills:</StyledLabel>
+            {filteredSkills &&
+                filteredSkills.map((skill) => (
+                    <StyledSkillContainer percentage={skill.skillLevel} key={skill.label}>
+                        <StyledLabel key={skill.skillLevel} />
+                        <StyledLabel key={skill.label}>{skill.label}</StyledLabel>
+                    </StyledSkillContainer>
+                ))}
         </StyledWrapper>
     );
 };
