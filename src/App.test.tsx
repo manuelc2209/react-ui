2e3fedd85373f2268afaa1f1d3209f36b2325d0c
electro2209 <20400660+manuelc2209@users.noreply.github.com>
2021-12-07 19:18:51 +0000

    React router workflow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import { App } from './App';
 
+const AppComponent = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
 it('renders snapshot correctly', () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = renderer.create(AppComponent).toJSON();
     expect(tree).toMatchSnapshot();
 });
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(AppComponent, div);
     ReactDOM.unmountComponentAtNode(div);
 });
