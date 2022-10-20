4121f9bc3ad1eb98f89f2b82ee30feb50a7387f4
electro2209 <20400660+manuelc2209@users.noreply.github.com>
2022-10-20 02:49:40 +0100

    Condition to DevTools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,8 +3,11 @@ import { useDispatch } from 'react-redux';
 
 import { reducers } from './reducer';
 
+const isProd = process.env.NODE_ENV !== 'production';
+
 export const store = configureStore({
-    reducer: reducers
+    reducer: reducers,
+    devTools: isProd
 });
 
 export type RootState = ReturnType<typeof store.getState>;
