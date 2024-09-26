81180bd743d611fa68bf9958b642b1afe7ffd8d6
Manuel Correia <electro2209@gmail.com>
2024-09-26 13:25:32 +0100

    Fix Dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,9 @@ export default defineConfig({
                         type: 'image/png'
                     }
                 ]
+            },
+            workbox: {
+                maximumFileSizeToCacheInBytes: 5 * 1024 ** 2
             }
         })
     ],
